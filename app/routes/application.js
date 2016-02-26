409c464908f5ae8858f8779ecff5eb561aa257f3
Ricardo Mendes <rokusu@gmail.com>
2016-02-26 02:13:57 +0000

    Adds missing semicolon. I'm sorry JSHint.

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,6 +4,6 @@ export default Ember.Route.extend({
   ajax: Ember.inject.service(),
 
   model() {
-    return this.get('ajax').request('https://26q4xbqcn2.execute-api.us-east-1.amazonaws.com/prod/getTombotData')
+    return this.get('ajax').request('https://26q4xbqcn2.execute-api.us-east-1.amazonaws.com/prod/getTombotData');
   }
 });
