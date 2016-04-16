cc37e3108fd453ee1451ba9ff21b64fe0032035a
Ricardo Mendes <rokusu@gmail.com>
2016-04-16 10:12:59 +0100

    bastfoot

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
+import fetch from "ember-network/fetch";
 
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
 
   model() {
-    return this.get('ajax').request('https://26q4xbqcn2.execute-api.us-east-1.amazonaws.com/prod/getTombotData')
+    return fetch('https://26q4xbqcn2.execute-api.us-east-1.amazonaws.com/prod/getTombotData')
+      .then(response => response.json())
       .then(data => {
         return Object.keys(data).map(key => {
           return { command: key, result: data[key] };
